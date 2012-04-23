3a56950c6283cd5f68418247ed11c9c83e9f3141
Alexander Bruy <alexander.bruy@gmail.com>
2012-04-23 20:17:32 +0300

    added translations

---
+++ i18n/reporter_ru_RU.ts
@@ -0,0 +1,198 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS>
+<TS version="2.0" language="ru_RU">
+<context>
+    <name>Reporter</name>
+    <message>
+        <location filename="reporter.py" line="73"/>
+        <source>Quantum GIS version detected: </source>
+        <translation>Обнаружена Quantum GIS версии: </translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="73"/>
+        <source>This version of Reporter requires at least QGIS version 1.0.0
+Plugin will not be enabled.</source>
+        <translation>Эта версия Reporter требует наличия QGIS 1.0.0 или старше
+Модуль не будет загружен.</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="79"/>
+        <source>Generates report</source>
+        <translation>Создает отчет</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="101"/>
+        <source>Reporter</source>
+        <translation>Reporter</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="106"/>
+        <source>About Reporter</source>
+        <translation>О Reporter</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="108"/>
+        <source>&lt;b&gt;Reporter&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Reporter&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="111"/>
+        <source>Version: %1</source>
+        <translation>Версия: %1</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="114"/>
+        <source>Generates report.</source>
+        <translation>Создает отчет.</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="115"/>
+        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Разработчики:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="117"/>
+        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
+        <translation>&lt;b&gt;Сайт:&lt;/b&gt;</translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="134"/>
+        <source>Close</source>
+        <translation>Закрыть</translation>
+    </message>
+</context>
+<context>
+    <name>ReporterDialog</name>
+    <message>
+        <location filename="reporterdialog.py" line="314"/>
+        <source>Reporter</source>
+        <translation>Reporter</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="25"/>
+        <source>Region(s) of interest</source>
+        <translation>Область интереса</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="37"/>
+        <source>Use only selected features</source>
+        <translation>Использовать только выделенные объекты</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="66"/>
+        <source>Layer name</source>
+        <translation>Имя слоя</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="74"/>
+        <source>Profile</source>
+        <translation>Профиль</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="83"/>
+        <source>New</source>
+        <translation>Новый</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="90"/>
+        <source>Load</source>
+        <translation>Загрузить</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="97"/>
+        <source>Save</source>
+        <translation>Сохранить</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="106"/>
+        <source>Create thematic maps</source>
+        <translation>Создать тематические карты</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="113"/>
+        <source>Also embed maps in report</source>
+        <translation>Также встроить карты в отчет</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="120"/>
+        <source>Load last configuration</source>
+        <translation>Загрузить последнюю конфигурацию</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="129"/>
+        <source>Report</source>
+        <translation>Отчет</translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="139"/>
+        <source>Browse</source>
+        <translation>Обзор</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="90"/>
+        <source>Select output directory</source>
+        <translation>Выберите каталог назначения</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="90"/>
+        <source>Micosoft Word 2003 (*.doc *.DOC)</source>
+        <translation>Micosoft Word 2003 (*.doc *.DOC)</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="107"/>
+        <source>Load configuration</source>
+        <translation>Загрузить конфигурацию</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="169"/>
+        <source>XML files (*.xml *.XML)</source>
+        <translation>XMLфайлы (*.xml *.XML)</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="128"/>
+        <source>Load error</source>
+        <translation>Ошибка загрузки</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="118"/>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation>Не удалось прочитать файл %1:
+%2.</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="128"/>
+        <source>Parse error at line %1, column %2:
+%3</source>
+        <translation>Ошибка разбора в строке %1, столбец %2:
+%3</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="169"/>
+        <source>Save configuration</source>
+        <translation>Сохранить конфигурацию</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="177"/>
+        <source>Save error</source>
+        <translation>Ошибка сохранения</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="177"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation>Не удалось записать файл %1:
+%2.</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="262"/>
+        <source>Please specify output report file</source>
+        <translation>Пожалуйста укажите файл отчета</translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="314"/>
+        <source>Completed!</source>
+        <translation>Завершено!</translation>
+    </message>
+</context>
+</TS>
---
+++ i18n/reporter_uk_UA.ts
@@ -0,0 +1,193 @@
+<?xml version="1.0" encoding="utf-8"?>
+<!DOCTYPE TS><TS version="1.1">
+<context>
+    <name>Reporter</name>
+    <message>
+        <location filename="reporter.py" line="73"/>
+        <source>Quantum GIS version detected: </source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="73"/>
+        <source>This version of Reporter requires at least QGIS version 1.0.0
+Plugin will not be enabled.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="79"/>
+        <source>Generates report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="101"/>
+        <source>Reporter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="106"/>
+        <source>About Reporter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="108"/>
+        <source>&lt;b&gt;Reporter&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="111"/>
+        <source>Version: %1</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="114"/>
+        <source>Generates report.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="115"/>
+        <source>&lt;b&gt;Developers:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="117"/>
+        <source>&lt;b&gt;Homepage:&lt;/b&gt;</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporter.py" line="134"/>
+        <source>Close</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+<context>
+    <name>ReporterDialog</name>
+    <message>
+        <location filename="reporterdialog.py" line="314"/>
+        <source>Reporter</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="25"/>
+        <source>Region(s) of interest</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="37"/>
+        <source>Use only selected features</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="66"/>
+        <source>Layer name</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="74"/>
+        <source>Profile</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="83"/>
+        <source>New</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="90"/>
+        <source>Load</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="97"/>
+        <source>Save</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="106"/>
+        <source>Create thematic maps</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="113"/>
+        <source>Also embed maps in report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="120"/>
+        <source>Load last configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="129"/>
+        <source>Report</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialogbase.ui" line="139"/>
+        <source>Browse</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="90"/>
+        <source>Select output directory</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="90"/>
+        <source>Micosoft Word 2003 (*.doc *.DOC)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="107"/>
+        <source>Load configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="169"/>
+        <source>XML files (*.xml *.XML)</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="128"/>
+        <source>Load error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="118"/>
+        <source>Cannot read file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="128"/>
+        <source>Parse error at line %1, column %2:
+%3</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="169"/>
+        <source>Save configuration</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="177"/>
+        <source>Save error</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="177"/>
+        <source>Cannot write file %1:
+%2.</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="262"/>
+        <source>Please specify output report file</source>
+        <translation type="unfinished"></translation>
+    </message>
+    <message>
+        <location filename="reporterdialog.py" line="314"/>
+        <source>Completed!</source>
+        <translation type="unfinished"></translation>
+    </message>
+</context>
+</TS>
